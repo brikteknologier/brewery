6ffe959a6b462b52e42283acb57dd1b783c59009
Helge Holm <helge.holm@gmail.com>
2024-05-29 15:22:57 +0200

    chore: Use non-deprecated URL parsing to shut up linters

--- proxy.js
+++ proxy.js
@@ -1,16 +1,11 @@
 var request = require('request');
 var _ = require('underscore');
-var url = require('url');
 
 module.exports = function(app, from, to) {
-  var endpoint = url.parse(to);
   var expr = "^" + from + "((/.*)?)$";
   app.all(new RegExp(expr), function(req, res) {
-    var url = _.clone(endpoint);
-    url.path = req.url;
-
     var apiRequest = request({
-      url: url,
+      url: new URL(req.url, to),
       method: req.method,
       headers: req.headers,
       followRedirect: false
